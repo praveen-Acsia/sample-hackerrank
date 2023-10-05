46fc190aca09bd941faae3d663c660d0fa3ab39a
praveen-Acsia <141117441+praveen-Acsia@users.noreply.github.com>
2023-10-05 17:35:28 +0530

    Update main.cpp

--- day-2/main.cpp
+++ day-2/main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 int min_play(vector<int> v){
     int total=0;
+    int player1=0;
+    int player2=0;
+    
     for(int i =0; i<v.size(); i++){
         if(v[i]==0){
             total--;
@@ -14,11 +17,11 @@ int min_play(vector<int> v){
             total++;
         }
     }
+    
     if(total<0){
         return 0;
     }
-    int player1=0;
-    int player2=0;
+
     for(int i =0; i<v.size(); i++){
         if(v[i]==0){
             player1--;
@@ -36,17 +39,16 @@ int min_play(vector<int> v){
 
 int main(){
     
-    
     int n;
     cin >> n;
     
     vector<int> v;
+    
     int val;
     for(int i =0; i<n; i++){
         cin >>val;
         v.push_back(val);
     }
-    
     cout<<min_play(v);
 }
 
