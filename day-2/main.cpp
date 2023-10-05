edbe58d7d6b627b9516865b488c1d7d746e5b48d
praveen-Acsia <141117441+praveen-Acsia@users.noreply.github.com>
2023-10-05 15:34:31 +0530

    Create main.cpp

---
+++ day-2/main.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int min_play(vector<int> v){
+    int total=0;
+    for(int i =0; i<v.size(); i++){
+        if(v[i]==0){
+            total--;
+        }
+        else{
+            total++;
+        }
+    }
+    int player1=0;
+    int player2=0;
+    for(int i =0; i<v.size(); i++){
+        if(v[i]==0){
+            player1--;
+        }
+        else{
+            player1++;
+        }
+        player2=total-player1;
+        if(player1>player2){
+            return i+1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    
+    
+    int n;
+    cin >> n;
+    
+    vector<int> v;
+    int val;
+    for(int i =0; i<n; i++){
+        cin >>val;
+        v.push_back(val);
+    }
+    
+    cout<<min_play(v);
+}
+
