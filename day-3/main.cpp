4e4397fed5ad799de6731135c1c995ebe2930771
praveen-Acsia <141117441+praveen-Acsia@users.noreply.github.com>
2023-10-09 11:36:47 +0530

    Update main.cpp

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -1,1 +1,26 @@
+include <iostream>
 
+
+int nsum(int a , int l, int d){
+    
+    float n= ((l-a)/d)+1;
+    int sum=(n/2)*(2*a +(n-1)*d);
+    return sum;
+}
+
+int main()
+{
+    int i,j,k;
+    std::cout<< "enter i : "<<std::endl;
+    std::cin >> i;
+    std::cout << "enter j : "<< std::endl;
+    std::cin >> j;
+    std::cout << "enter k : "<<std::endl;
+    std::cin >> k;
+    
+    int sum=nsum(i,j,1);
+    sum+=nsum(j-1,k,-1);
+    std::cout << sum;
+
+    return 0;
+}
