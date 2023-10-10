79e9f56fe8b92d5c82ba5d80e82825e914776653
praveen-Acsia <141117441+praveen-Acsia@users.noreply.github.com>
2023-10-10 15:57:38 +0530

    Create q3.cpp

---
+++ day-4/q3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+
+string decript(string& encript, int& k) {
+	int curr;
+	string decript;
+	for (int i = 0; i < encript.length(); i++) {
+		curr = (int)encript[i] - k;
+		while (curr < 65) {
+		    
+				curr = 90 -(65-curr)+1 ;
+		}
+		decript.push_back((char)curr);
+	}
+	return decript;
+
+}
+
+int main() {
+
+	string encript;
+	int k;
+
+	cout << "enter encripted string : " << endl;
+	cin >> encript;
+	cout << "enter k" << endl;
+	cin >> k;
+
+	cout << decript(encript, k);
+
+}
