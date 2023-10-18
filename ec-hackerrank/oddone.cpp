0bbebd131ff17703c713446a11da0e66e920e34f
praveen-Acsia <141117441+praveen-Acsia@users.noreply.github.com>
2023-10-18 12:31:17 +0530

    Create oddone.cpp

---
+++ ec-hackerrank/oddone.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+string findOdd(vector<string>& arr) {
+	int diff;
+	vector<vector<int>> vals;
+	vector<int>val;
+	string oddone="";
+
+	for (int i = 0; i < arr.size(); i++) {
+		for (int j = 0; j < arr[i].length()-1; j++) {
+			diff = arr[i][j + 1] - arr[i][j];
+			val.push_back(diff);
+		}
+		vals.push_back(val);
+		val.clear();
+	}
+	for (int i = 0; i < vals.size(); i++) {
+		if (count(vals.begin(), vals.end(), vals[i]) == 1) {
+			oddone = arr[i];
+			return oddone;
+		}
+	}
+	return oddone;
+}
+
+int main() {
+
+	int size;
+	string ele;
+	vector<string> arr;
+
+	cout << "enter no of strings : " << endl;
+	cin >> size;
+	cout << "enter strings : " << endl;
+	for (int i = 0; i < size; i++) {
+		cin >> ele;
+		arr.push_back(ele);
+	}
+	cout << findOdd(arr);
+
+}
